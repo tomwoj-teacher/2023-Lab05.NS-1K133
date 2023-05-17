7c1fc9f267dd8ba54676aeaea2cde4bd171d1334
tkupczyk <131359505+ToKu88@users.noreply.github.com>
2023-05-17 14:45:40 +0200

    Create Script-9.bash

---
+++ bash/Script-9.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Script 9: Add user to the group
+
+    read -p "Enter user name: " username
+
+		if ! id "$username" >/dev/null 2>&1; then
+		    echo "\nUser named $username alredy exists.\n"
+		    exit 1
+		fi
+
+		read -p "\nEnter group name: " groupname
+
+		if ! grep -q "^$groupname:" /etc/group; then
+		    echo "\nGroup named $groupname doesn't exist.\n"
+		    exit 1
+		fi
+
+		usermod -aG "$groupname" "$username"
+
+		echo "\nUser $username was successfully added to $groupname.\n"
