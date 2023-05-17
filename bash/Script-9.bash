5797d531d882ea00d77bde0aff9676e6ef11730b
tkupczyk <131359505+tkupczyk@users.noreply.github.com>
2023-05-17 15:10:23 +0200

    Update Script-9.bash

--- bash/Script-9.bash
+++ bash/Script-9.bash
@@ -4,17 +4,17 @@
     read -p "Enter user name: " username
 
 		if ! id "$username" >/dev/null 2>&1; then
-		    echo "\nUser named $username doesn't exists.\n"
+		    echo "User named $username doesn't exists."
 		    exit 1
 		fi
 
 		read -p "\nEnter group name: " groupname
 
 		if ! grep -q "^$groupname:" /etc/group; then
-		    echo "\nGroup named $groupname doesn't exist.\n"
+		    echo "Group named $groupname doesn't exist."
 		    exit 1
 		fi
 
 		usermod -aG "$groupname" "$username"
 
-		echo "\nUser $username was successfully added to $groupname.\n"
+		echo "User $username was successfully added to $groupname."
